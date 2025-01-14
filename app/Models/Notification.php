080da6ac2efa0b3117fe1ee5604e2fd8c9dbbe24
KarimMenem <karimmenem@Karims-MacBook-Pro.local>
2025-01-14 12:13:39 +0200

    fix

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'notification_id'; // Set the primary key
+    public $incrementing = true; // Ensure primary key auto-increments
+    protected $keyType = 'int'; // Set the primary key type
+
+    protected $fillable = [
+        'user_id',
+        'message',
+        'is_read',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
